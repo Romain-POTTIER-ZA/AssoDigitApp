58692351037432d5b60fc516333fb0feec98e240
Kiwiks2000 <107352658+Kiwiks2000@users.noreply.github.com>
2025-07-26 00:28:06 +0200

    maj

---
+++ dashboard.php
@@ -0,0 +1,45 @@
+<?php
+
+
+$allowedPages = [
+    'home'
+];
+
+$page = $_GET['page'] ?? 'home';
+
+if (!in_array($page, $allowedPages)) {
+    
+    $page = 'home';
+}
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once 'modules/settings/link.php'; ?>
+    <title>AssoDigit</title>
+</head>
+
+<body>
+    <nav>
+
+    </nav>
+
+    <aside>
+
+    </aside>
+
+
+    <main>
+        <?php include("./pages/$page.php"); ?>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+
+</html>
